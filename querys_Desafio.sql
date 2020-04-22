bbed4db22451a20f66b0982adfb2d70bd759ec49
tkmaster1 <45917644+tkmaster1@users.noreply.github.com>
2020-04-22 17:24:49 -0300

    Upload de arquivos do projeto de teste
    Teste de implementação.

---
+++ querys_Desafio.sql
@@ -0,0 +1,8 @@
+﻿select * from Livro
+select * from autor
+select * from assunto
+select * from livroassunto 
+select * from livroautor
+
+ --update livro set DataExclusao = null, Ativo = 'S' where LivroId = 5
+ --update autor set DataExclusao = null, Ativo = 'S' where AutorId = 11
